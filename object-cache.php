48935ae2059555615d98565ed92e935114bf20a1
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-03-11 20:18:10 +0200

    Initial commit

---
+++ object-cache.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+Plugin Name: Memcached
+Description: Memcache or Memcached backend for the WP Object Cache
+Version: 0.1
+Plugin URI: https://github.com/bonny/memcachy/
+Author: Vasyl Sokolyk
+
+*/
