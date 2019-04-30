53c3a5f2ee835de8d239dcde42aed91b18bf8e4c
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-04-30 11:46:27 +0300

    removed OutputCache class

--- object-cache.php
+++ object-cache.php
@@ -1,7 +1,6 @@
 <?php
 
 use JazzMan\WPObjectCache\DriverAdapter;
-use JazzMan\WPObjectCache\OutputCache;
 
 /**
  * @return bool
@@ -31,7 +30,7 @@ function wp_cache_add($key, $value, string $group = 'default', $expiration = 0)
  *
  * @return bool
  */
-function wp_cache_decr($key, $offset = 1, string $group = 'default')
+function wp_cache_decr($key, int $offset = 1, string $group = 'default')
 {
     return wp_object_cache()->decrement($key, $offset, $group);
 }
@@ -77,7 +76,7 @@ function wp_cache_get($key, string $group = 'default', $force = false, &$found =
  *
  * @return bool
  */
-function wp_cache_incr($key, $offset = 1, string $group = 'default')
+function wp_cache_incr($key, int $offset = 1, string $group = 'default')
 {
     return wp_object_cache()->increment($key, $offset, $group);
 }
@@ -142,132 +141,24 @@ function wp_cache_add_non_persistent_groups($groups)
 }
 
 /**
- * @param string $key
- * @param string $group
- *
- * @return bool
- */
-function wp_cache_has(string $key, string $group = 'default'){
-   return wp_object_cache()->has($key,$group);
-}
-
-
-/**
- * @param array  $keys
- * @param string $group
- *
- * @return array
- */
-function wp_cache_sanitize_keys(array $keys, string $group = 'default'){
-   return wp_object_cache()->sanitizeKeys($keys,$group);
-}
-
-/**
- * @param string $key
- * @param string $group
- *
- * @return string
- */
-function wp_cache_sanitize_key(string $key, string $group = 'default'){
-    return wp_object_cache()->sanitizeKey($key,$group);
-}
-
-/**
  * Sets up Object Cache Global and assigns it.
  */
 function wp_cache_init()
 {
-    wp_object_cache_init();
+    global $wp_object_cache;
+
+    if (! ($wp_object_cache instanceof DriverAdapter)) {
+        $wp_object_cache = new DriverAdapter;
+    }
 }
+
 
 /**
  * @return DriverAdapter
  */
 function wp_object_cache()
 {
-    if (!isset($GLOBALS['wp_object_cache'])) {
-        wp_object_cache_init();
-    }
+    global $wp_object_cache;
 
-    return $GLOBALS['wp_object_cache'];
-}
-
-/**
- * Sets up Output Cache Global and assigns it.
- */
-function wp_object_cache_init()
-{
-    $GLOBALS['wp_object_cache'] = new DriverAdapter();
-}
-
-/**
- * Sets up Output Cache Global and assigns it.
- */
-function wp_output_cache_init()
-{
-    $GLOBALS['wp_output_cache'] = new OutputCache();
-}
-
-/**
- * Returns the Output Cache Global.
- *
- *
- * @return OutputCache
- */
-function wp_output_cache()
-{
-    if (!isset($GLOBALS['wp_output_cache'])) {
-        wp_output_cache_init();
-    }
-
-    return $GLOBALS['wp_output_cache'];
-}
-
-/**
- * Should we skip the output cache?
- *
- *
- * @return bool
- */
-function wp_skip_output_cache()
-{
-    // Bail if caching not turned on
-    if (!defined('WP_CACHE') || (true !== WP_CACHE)) {
-        return true;
-    }
-
-    // Bail if no content directory
-    if (!defined('WP_CONTENT_DIR')) {
-        return true;
-    }
-
-    // Never cache interactive scripts or API endpoints.
-    if (in_array(basename($_SERVER['SCRIPT_FILENAME']), [
-        'wp-app.php',
-        'wp-cron.php',
-        'ms-files.php',
-        'xmlrpc.php',
-    ])) {
-        return true;
-    }
-
-    // Never cache JavaScript generators
-    if (strstr($_SERVER['SCRIPT_FILENAME'], 'wp-includes/js')) {
-        return true;
-    }
-
-    // Never cache when POST data is present.
-    if (!empty($GLOBALS['HTTP_RAW_POST_DATA']) || !empty($_POST)) {
-        return true;
-    }
-
-    return false;
-}
-
-/**
- * Cancel Output-Cache.
- */
-function wp_output_cache_cancel()
-{
-    wp_output_cache()->cancel = true;
+    return $wp_object_cache;
 }
