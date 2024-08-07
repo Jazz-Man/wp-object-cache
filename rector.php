358fc1a22c32879008362b153253ed6f48f1d3c2
Vasyl Sokolyk <vsokolyk@gmail.com>
2024-08-07 12:54:31 +0300

    build(style): update rector config

--- rector.php
+++ rector.php
@@ -6,43 +6,39 @@ use Rector\CodeQuality\Rector\Empty_\SimplifyEmptyCheckOnEmptyArrayRector;
 use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
 use Rector\Config\RectorConfig;
 use Rector\Php71\Rector\FuncCall\RemoveExtraParametersRector;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
 
-return static function ( RectorConfig $config ): void {
-    $config->sets( [
-        SetList::CODE_QUALITY,
-        SetList::CODING_STYLE,
-        SetList::TYPE_DECLARATION,
-        SetList::EARLY_RETURN,
-        SetList::NAMING,
-        SetList::PRIVATIZATION,
-        LevelSetList::UP_TO_PHP_82,
-    ] );
-
-    $config->fileExtensions( ['php'] );
-    $config->importNames();
-    $config->removeUnusedImports();
-    $config->importShortClasses( false );
-    $config->parallel();
-    $config->phpstanConfig( __DIR__.'/phpstan-rector.neon' );
-
-    $config->paths( [
+return RectorConfig::configure()
+    ->withPhpSets( php82: true )
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        codingStyle: true,
+        typeDeclarations: true,
+        privatization: true,
+        naming: true,
+        instanceOf: true,
+        earlyReturn: true,
+        strictBooleans: true,
+        rectorPreset: true
+    )
+    ->withPaths( [
         __DIR__.'/include',
         __DIR__.'/src',
         __DIR__.'/wp-object-cache.php',
-    ] );
-
-    $config->skip( [
+    ] )
+    ->withPhpPolyfill()
+    ->withFileExtensions( [ 'php' ] )
+    ->withSkip( [
         __DIR__.'/vendor',
-        __DIR__.'/cache',
-        //		CallableThisArrayToAnonymousFunctionRector::class,
-        //        ClassConstantToSelfClassRector::class,
         RemoveExtraParametersRector::class,
         EncapsedStringsToSprintfRector::class,
         DisallowedEmptyRuleFixerRector::class,
         SimplifyEmptyCheckOnEmptyArrayRector::class,
-        //	    ArrayShapeFromConstantArrayReturnRector::class,
-    ] );
-};
+    ] )
+    ->withParallel()
+    ->withPHPStanConfigs( [
+        __DIR__.'/phpstan-rector.neon',
+    ] )
+    ->withImportNames( importShortClasses: false, removeUnusedImports: true )
+;
